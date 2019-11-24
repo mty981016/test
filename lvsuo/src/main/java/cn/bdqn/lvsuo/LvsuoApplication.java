82e4208b226852d1d7fb5286e332eaecda5e1822
mty981016 <2329266693@qq.com>
2019-11-24 19:29:23 +0800

    新建springBoot项目

---
+++ lvsuo/src/main/java/cn/bdqn/lvsuo/LvsuoApplication.java
@@ -0,0 +1,13 @@
+package cn.bdqn.lvsuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvsuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvsuoApplication.class, args);
+    }
+
+}
---
+++ lvsuo/src/test/java/cn/bdqn/lvsuo/LvsuoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bdqn.lvsuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvsuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
