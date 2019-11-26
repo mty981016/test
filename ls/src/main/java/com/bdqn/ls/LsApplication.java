e1233a10b9283ddddb459289e0fc10d5c59b795d
mty981016 <2329266693@qq.com>
2019-11-26 12:05:21 +0800

    新建项目

---
+++ ls/src/main/java/com/bdqn/ls/LsApplication.java
@@ -0,0 +1,13 @@
+package com.bdqn.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsApplication.class, args);
+    }
+
+}
---
+++ ls/src/test/java/com/bdqn/ls/LsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
